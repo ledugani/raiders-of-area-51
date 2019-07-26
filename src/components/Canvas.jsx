1f262c7342be4d69e11e5c08584939e4b1ef0aec
Tom Dugan <thomas@aadwebb.com>
2019-07-26 17:03:14 -0500

    update Auth0 info

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -8,10 +8,22 @@ import CurrentScore from './CurrentScore'
 import FlyingObject from './FlyingObject';
 import StartGame from './StartGame';
 import Title from './Title';
+import Leaderboard from './Leaderboard';
+import { signIn } from 'auth0-web';
 
 const Canvas = (props) => {
   const gameHeight = 1200;
   const viewBox = [window.innerWidth / -2, 100 - gameHeight, window.innerWidth, gameHeight];
+  const leaderboard = [
+    { id: 'd4', maxScore: 82, name: 'Ado Kukic', picture: 'https://twitter.com/KukicAdo/photo', },
+    { id: 'a1', maxScore: 235, name: 'Bruno Krebs', picture: 'https://twitter.com/brunoskrebs/photo', },
+    { id: 'c3', maxScore: 99, name: 'Diego Poza', picture: 'https://twitter.com/diegopoza/photo', },
+    { id: 'b2', maxScore: 129, name: 'Jeana Tahnk', picture: 'https://twitter.com/jeanatahnk/photo', },
+    { id: 'e5', maxScore: 34, name: 'Jenny Obrien', picture: 'https://twitter.com/jenny_obrien/photo', },
+    { id: 'f6', maxScore: 153, name: 'Kim Maida', picture: 'https://twitter.com/KimMaida/photo', },
+    { id: 'g7', maxScore: 55, name: 'Tom Dugan', picture: 'https://twitter.com/LeDugani/photo', },
+    { id: 'h8', maxScore: 146, name: 'Sebastian Peyrott', picture: 'https://twitter.com/speyrott/photo', },
+  ];
   return (
     <svg
       id="aliens-go-home-canvas"
@@ -41,6 +53,14 @@ const Canvas = (props) => {
         <g>
           <FlyingObject position={{x: -150, y: -300}}/>
           <FlyingObject position={{x: 150, y: -300}}/>
+        </g>
+      }
+
+      { ! props.gameState.started &&
+        <g>
+          <StartGame onClick={() => props.startGame()} />
+          <Title />
+          <Leaderboard currentPlayer={leaderboard[6]} authenticate={signIn} leaderboard={leaderboard} />
         </g>
       }
 
